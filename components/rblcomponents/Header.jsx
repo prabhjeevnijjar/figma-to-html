88c6028fa29b6ce23beff6ee5423b90b59871dd2
psn <psn@psns-MacBook-Air.local>
2023-10-08 14:04:26 +0530

    header for rbl done

---
+++ components/rblcomponents/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <header className="rbl-header">
+      <div className="rbl-header__logo">
+        <img src="/rblbank/rbl-logo.png" className="" />
+      </div>
+      <div className="rbl-header__ham">
+        <div className="rbl-header__ham-txt">
+          <div>Prabhjeev</div>
+          <div>Available Points: 3000</div>
+        </div>
+        <img src="/rblbank/Frame.png" className="" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ pages/rblbank/brands.js
@@ -0,0 +1,11 @@
+import Header from '@/components/rblcomponents/Header';
+
+const brandPage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default brandPage;
